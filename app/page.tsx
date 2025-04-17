0a91c6ae5af2e0bcc7e5e1fab4bf9d1a29d2af38
cm-nakagawa <snakagawax27rky2@gmail.com>
2025-04-17 11:35:02 +0900

    fix: css

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import "./../app/app.css";
+; import "./../app/app.css";
 import { Amplify } from "aws-amplify";
 import outputs from "@/amplify_outputs.json";
 import "@aws-amplify/ui-react/styles.css";
