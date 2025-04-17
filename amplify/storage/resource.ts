0283359660ae980a7f1f84e40298469456efaf4a
cm-nakagawa <snakagawax27rky2@gmail.com>
2025-04-17 11:13:58 +0900

    add storage browser

--- amplify/storage/resource.ts
+++ amplify/storage/resource.ts
@@ -1,18 +1,15 @@
-import { defineStorage } from "@aws-amplify/backend";
+import { defineStorage } from '@aws-amplify/backend';
 
 export const storage = defineStorage({
-    name: 'myProjectFiles',
+    name: 'amplifyTeamDrive',
     access: (allow) => ({
-        'public/*': [
+        'profile-pictures/{entity_id}/*': [
             allow.guest.to(['read']),
-            allow.authenticated.to(['read', 'write', 'delete']),
-        ],
-        'protected/{entity_id}/*': [
-            allow.authenticated.to(['read']),
             allow.entity('identity').to(['read', 'write', 'delete'])
         ],
-        'private/{entity_id}/*': [
-            allow.entity('identity').to(['read', 'write', 'delete'])
-        ]
+        'picture-submissions/*': [
+            allow.authenticated.to(['read', 'write']),
+            allow.guest.to(['read', 'write'])
+        ],
     })
 });
