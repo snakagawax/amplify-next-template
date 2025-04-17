5a33d86a118840ce8804064d0fb99a63496783f9
cm-nakagawa <snakagawax27rky2@gmail.com>
2025-04-17 11:27:23 +0900

    fix: pages

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,23 @@
 "use client";
 
-import "@aws-amplify/ui-react/styles.css";
-import "@aws-amplify/ui-react-storage/storage-browser-styles.css";
-import outputs from "@/amplify_outputs.json";
-import { Authenticator, Button } from "@aws-amplify/ui-react";
-import { StorageBrowser } from "@aws-amplify/ui-react-storage";
+import "./../app/app.css";
 import { Amplify } from "aws-amplify";
+import outputs from "@/amplify_outputs.json";
+import "@aws-amplify/ui-react/styles.css";
+import {
+  createAmplifyAuthAdapter,
+  createStorageBrowser,
+} from '@aws-amplify/ui-react-storage/browser';
 
 Amplify.configure(outputs);
 
+const { StorageBrowser } = createStorageBrowser({
+  config: createAmplifyAuthAdapter(),
+});
+
 export default function App() {
+
   return (
-    <Authenticator hideSignUp={true}>
-      {({ signOut }) => (
-        <>
-          <Button onClick={signOut}>Sign Out</Button>
-          <StorageBrowser />
-        </>
-      )}
-    </Authenticator>
+    <StorageBrowser />
   );
 }
